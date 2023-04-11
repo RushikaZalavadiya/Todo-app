fe12682ed7935ce2c5b79f915cc88e819fd6874a
nishavadadoriya3 <vadadoriyanisha@gmail.com>
2023-04-11 22:46:24 +0530

    --ad full page add.....

--- src/app/modules/update/update.page.ts
+++ src/app/modules/update/update.page.ts
@@ -25,49 +25,66 @@ export class UpdatePage implements OnInit {
     toast.present()
   }
   //a show kare chhe banner add ne.....
-  showbaaner() {
-    AdmobAds.showBannerAd({
-      adId: "ca-app-pub-3940256099942544/6300978111", isTesting: true,
-      adSize: BannerSize.BANNER, adPosition: BannerPosition.BOTTOM
-    }).then(() => {
-      console.log('Banner Ad Shown');
-      this.preserntToas("banner AD shown")
-    }).catch(err => {
-      console.log(err.message);
-      this.preserntToas(err.message)
-    });
+  // showbaaner() {
+  //   AdmobAds.showBannerAd({
+  //     adId: "ca-app-pub-3940256099942544/6300978111", isTesting: true,
+  //     adSize: BannerSize.BANNER, adPosition: BannerPosition.BOTTOM
+  //   }).then(() => {
+  //     console.log('Banner Ad Shown');
+  //     this.preserntToas("banner AD shown")
+  //   }).catch(err => {
+  //     console.log(err.message);
+  //     this.preserntToas(err.message)
+  //   });
 
-  }
+  // }
   //a hide kare chhe banner ad ne....
-  hidebanner() {
-    AdmobAds.hideBannerAd().then(() => {
-      console.log('Banner Ad Hidden')
-      this.preserntToas("Hidden")
+  // hidebanner() {
+  //   AdmobAds.hideBannerAd().then(() => {
+  //     console.log('Banner Ad Hidden')
+  //     this.preserntToas("Hidden")
 
-    }).catch(err => {
-      console.log(err.message);
-      this.preserntToas("banner AD Hidden")
+  //   }).catch(err => {
+  //     console.log(err.message);
+  //     this.preserntToas("banner AD Hidden")
 
-    });
+  //   });
 
-  }
+  // }
 
   //a chhe ne resume kare chhe banner ad ne
-  resumebanner() {
-    AdmobAds.resumeBannerAd().then(() => {
-      console.log('Banner Ad Resumed');
-    }).catch(err => {
-      console.log(err.message);
-    });
+  // resumebanner() {
+  //   AdmobAds.resumeBannerAd().then(() => {
+  //     console.log('Banner Ad Resumed');
+  //   }).catch(err => {
+  //     console.log(err.message);
+  //   });
 
-  }
+  // }
   //a chhe ne remove kare chhe banner ad ne
-  removebannerad() {
-    AdmobAds.removeBannerAd().then(() => {
-      console.log('Banner Ad Removed');
-    }).catch(err => {
-      console.log(err.message);
-    });
+  // removebannerad() {
+  //   AdmobAds.removeBannerAd().then(() => {
+  //     console.log('Banner Ad Removed');
+  //   }).catch(err => {
+  //     console.log(err.message);
+  //   });
 
+  // }
+  loadinterad() {
+    AdmobAds.loadInterstitialAd({
+      adId: 'ca-app-pub-3940256099942544/1033173712',
+      isTesting: true
+    }).then(() => {
+      this.preserntToas("ad load.")
+    }).catch((err) => {
+      this.preserntToas(err.message)
+    })
+  }
+  showinterad() {
+    AdmobAds.showInterstitialAd().then(() => {
+      this.preserntToas("......");
+    }).catch((err) => {
+      this.preserntToas(err.message)
+    })
   }
 }
